217c12d52bd87f641215ee7dd3abb76a494892c8
caleb-tolu <israel.fabulous@gmail.com>
2018-11-15 11:01:33 +0100

    Space X data GraphQL

---
+++ public/precache-manifest.8926d70948cf755a7fe8f5a891d48986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "386d3c92a527e90224a2",
+    "url": "/static/js/main.386d3c92.chunk.js"
+  },
+  {
+    "revision": "2ae01d2d6cdbe41e8fc4",
+    "url": "/static/js/1.2ae01d2d.chunk.js"
+  },
+  {
+    "revision": "386d3c92a527e90224a2",
+    "url": "/static/css/main.35cf2a8c.chunk.css"
+  },
+  {
+    "revision": "19d951b3e16e793fc065c551c9b3f5f4",
+    "url": "/index.html"
+  }
+];
